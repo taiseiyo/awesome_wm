962a3762b5aa221a0bce4b0f0455ee7b2478f3b7
suzuki taisei <taisei@localhost>
2021-06-10 23:37:49 +0900

    fix

--- rc.lua
+++ rc.lua
@@ -53,7 +53,10 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/theme.lua")
+
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt -e tmux"
