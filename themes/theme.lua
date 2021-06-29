a580adf4ad6769ca01c905ca47b7e9d2ba095994
suzuki taisei <taisei@localhost>
2021-06-30 00:28:27 +0900

    fix

--- themes/theme.lua
+++ themes/theme.lua
@@ -19,9 +19,9 @@ theme.bg_urgent     = "#ff0000"
 -- theme.bg_minimize   = "#444444"
 theme.bg_minimize   = "#a30000"
 theme.bg_systray    = theme.bg_normal
-
 theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
+-- theme.fg_focus      = "#ffffff"
+theme.fg_focus      = "#00ff00"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
