a038fb2e818fee67d17ffd83ed1f47a0c1425077
suzuki taisei <taisei@localhost>
2022-03-11 17:15:07 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt update
 sudo apt list --upgradable
-sudo apt install awesome lua5.4 emacs scrot xclip git brightnessctl xfonts-terminus acpi tmux
+sudo apt install awesome lua5.4 emacs scrot xclip git brightnessctl xfonts-terminus acpi tmux rxvt-unicode
 git clone https://github.com/taiseiyo/awesome_wm.git
 cd awesome_wm
 cp xinitrc ~/.xinitrc
